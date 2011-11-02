06c742588f9af5513cc549c58307b6637b861e25
dlte <manellermus@gmail.com>
2011-11-02 22:18:03 +0100

    removed threads

--- convert.php
+++ convert.php
@@ -10,7 +10,7 @@ $xml = $video->getClipInfo($_GET['id']);
 $flv = $xml->video_file_url;
 $title = $video->parseTitle($xml->title).'_part'.$_GET['part'].'_'.$_GET['id'];
 if(!is_dir("converted")) { @mkdir("converted",0777); }
-exec("ffmpeg.exe -threads 8 -i ".$flv." -vcodec wmv2 -sameq -acodec wmav2 -f asf converted/{$title}.asf  1>log.txt 2>&1", $output);
+exec("ffmpeg.exe -i ".$flv." -vcodec wmv2 -sameq -acodec wmav2 -f asf converted/{$title}.asf  1>log.txt 2>&1", $output);
 exec("ffmpeg.exe -itsoffset -4  -i converted/{$title}.asf -vcodec mjpeg -vframes 1 -an -f rawvideo -s 1024x600 converted/{$title}.jpg",$output);
 }
 elseif($_GET['id']=="xsplit")
